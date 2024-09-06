28069cd3bcbc55aff1b32e842cd78a50bfb1175f
Lukas <49550361+Ahrirg@users.noreply.github.com>
2024-09-06 10:33:59 +0300

    UPDATED
    with thinkerer :'3

--- Main.py
+++ Main.py
@@ -1,11 +1,72 @@
-# import spotdl
 import os, shutil
 import yt_dlp
 from spotdl import Spotdl
 from mutagen.easyid3 import EasyID3
+from tkinter import *
 
-Client_id_spotify = "47eb5313f7bf40339fc5cfb2c9186d13"
-Client_secret_spotify = "6e4a2415ddd542d280e8250063553e27"
+Client_id_spotify = ""
+Client_secret_spotify = ""
+
+PathofCode = os.path.dirname(__file__)
+CMDpath = os.getcwd()
+
+Putbackthismusic = []
+
+print("""\n\n
+      ########       ###     #########    ########            ###    ###                ########    ###  
+     ###    ###   ### ###   ###     ### ###    ###          #####  #####               ###    ###  ###   
+    ###    ###  ###   ###  ###     ### ###                ###  #####  ###             ###    ###  ###    
+   ###    ### ########### #########   ###                ###   ###   ###   #######   ###    ###  ###     
+  ###    ### ###     ### ###     ### ###   #####        ###         ###             ###    ###  ###      
+ ###    ### ###     ### ###     ### ###     ###        ###         ###             ###    ###  ###       
+########   ###     ### ###     ###  ##########        ###         ###             ########    ########## \n\n""")
+
+
+
+master = Tk()
+Label(master, text='List name').grid(row=0)
+Label(master, text='Album name').grid(row=1)
+master.title('Offbrand spotify')
+
+e1 = Text(master)
+e2 = Entry(master, width=50)
+
+
+ListGuiData = ''
+AlbumName = ''
+
+def SAVETOVARIABLE():
+    global ListGuiData
+    global AlbumName
+    
+    ListGuiData = e1.get("1.0",END)
+    AlbumName = e2.get()
+
+    master.destroy()
+
+e3 = Button(master, text='Download', width=50, command=SAVETOVARIABLE)
+e1.grid(row=0, column=1, pady=5)
+e2.grid(row=1, column=1, pady=7)
+e3.grid(row=2, column=1, pady=7)
+
+mainloop()
+
+
+f = open(f'{PathofCode}/List.txt', "w")
+f.write(ListGuiData)
+f.close()
+
+FolderN = AlbumName
+
+if FolderN == "":
+    FolderN = "Unnamed"
+
+FolderPath = os.path.join(PathofCode, FolderN)
+
+try:
+    os.mkdir(FolderPath)
+except:
+    print('\n//---//---//---//\nFolder Was already made\n//---//---//---//\n')
 
 
 def SpotifySideDownload(List):
@@ -15,8 +76,9 @@ def SpotifySideDownload(List):
     song_objs = Sdl.search(List)
 
     for x in song_objs:
-        print(x.name, end=", ")
-
+        # print(x.name, end=", ")
+        Putbackthismusic.append(f'{x.artist} - {x.name}.mp3')
+        
     Sdl.download_songs(song_objs)
     print("Spotify download has finished")
 
@@ -25,6 +87,8 @@ def YoutubeSideDownload(List):
         'format': 'bestaudio/best',
         'skip-unavailable-fragments': True,
         'addmetadata': True,
+        'outtmpl': f'{PathofCode}/%(title)s.%(ext)s',
+
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -37,47 +101,16 @@ def YoutubeSideDownload(List):
 
 
 
-print("""\n\n
-      ########       ###     #########    ########            ###    ###                ########    ###  
-     ###    ###   ### ###   ###     ### ###    ###          #####  #####               ###    ###  ###   
-    ###    ###  ###   ###  ###     ### ###                ###  #####  ###             ###    ###  ###    
-   ###    ### ########### #########   ###                ###   ###   ###   #######   ###    ###  ###     
-  ###    ### ###     ### ###     ### ###   #####        ###         ###             ###    ###  ###      
- ###    ### ###     ### ###     ### ###     ###        ###         ###             ###    ###  ###       
-########   ###     ### ###     ###  ##########        ###         ###             ########    ########## \n\n""")
-
-PathofCode = os.getcwd()
-
-print(os.listdir(PathofCode))
-
-FolderN = input('Custom Album name: ')
-
-if FolderN == "":
-    FolderN = "Unnamed"
-
-FolderPath = os.path.join(PathofCode, FolderN)
-
-try:
-    os.mkdir(FolderPath)
-except:
-    print('\n//---//---//---//\nError has been cought\n//---//---//---//\n')
-    shutil.rmtree(FolderPath)
-    os.mkdir(FolderPath)
-
-# f = open('List.txt', "a")
-# f.close()
-
-
 fowards = False
 while fowards == False:
-    inputQuary = input('(If blank will try use "List.txt" and download all of the music from that) -=- Music name or url: ')
+    inputQuary = ""
 
     if inputQuary == "":
-        f = open('List.txt', "r")
+        f = open(f'{PathofCode}/List.txt', "r")
         data = f.readlines()
         f.close()
         if data != []:
-            print(data)
+            # print(data)
             inputQuary = data
             fowards = True
     else:
@@ -101,19 +134,47 @@ for x in inputQuary:
         y = x.replace('\n', '')
         NameSongs.append(y)
 
+
 # print(f'Filtered songs//\nYoutube: {YoutubeSongs}\nSpotify: {SpotifySongs}\nNameSongs: {NameSongs}')
+try:
+    YoutubeSideDownload(YoutubeSongs)
+except Exception as err:
+    print(f"ERROR WAS : {err}")
 
-SpotifySideDownload(SpotifySongs)
-YoutubeSideDownload(YoutubeSongs)
+for x in Putbackthismusic:
+    try: 
+        shutil.move(f"{CMDpath}/{x}", f"{PathofCode}/{x}")
+    except:
+        print('Buvo error no wories tho')
 
-for song in os.listdir(PathofCode):
-    if '.mp3' in song:
-        audio = EasyID3(f"{song}")
-        audio['album'] = f"{FolderN}"
-        audio['albumartist'] = f"{FolderN}"
-        audio.save()
+try:
+    SpotifySideDownload(SpotifySongs)
+except Exception as err:
+    print(f"ERROR WAS : {err}")
 
-        print(audio)
 
-        shutil.move(f"{PathofCode}/{song}", f"{PathofCode}/{FolderN}/{song}")
-        
+# print(os.listdir(PathofCode))
+try:
+    for song in os.listdir(PathofCode):
+        # print(song)
+        if '.mp3' in song:
+            audio = EasyID3(f"{PathofCode}/{song}")
+            audio['album'] = f"{FolderN}"
+            audio['albumartist'] = f"{FolderN}"
+            audio.save()
+
+            # print(audio)
+
+            shutil.move(f"{PathofCode}/{song}", f"{PathofCode}/{FolderN}/{song}")
+except Exception as err:
+    print(f"ERROR WAS : {err}")
+
+print('-=- Press enter to Finish -=-')
+
+masterFN = Tk()
+masterFN.title('Offbrand spotify')
+Label(masterFN, text='Finished').grid(row=0)
+e3 = Button(masterFN, text='Done', width=50, command=masterFN.destroy)
+e3.grid(row=1, column=1)
+mainloop()
+masterFN.destroy
